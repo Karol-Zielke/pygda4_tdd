64c9306d682b9299c6c523ed7e9fded6d510f631
karol <karol.zielke@gmail.com>
2019-12-03 21:16:06 +0100

    Wprowadziłem poprawki, jeden test wciąż nie działa. Reszta działa, choc nie dokońca rozumiem dlaczego ;)

--- time_table/tests/time_table_test.py
+++ time_table/tests/time_table_test.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import unittest
 from random import randint
 from hour.hour import Hour
 from time_table.tests.hour_stub import HourStub
@@ -6,27 +6,29 @@ from time_table.time_table import TimeTable
 import mock
 
 
-class TimeTableTest(TestCase):
-    # def setUp(self) -> None:
-    #     self.time_table = TimeTable((2345, 4567, 8678))
-    #
-    # def tearDown(self) -> None:
-    #     del self.time_table
+class TimeTableTest(unittest.TestCase):
 
-    # def test_should_return_departure_times(self):
-    #     self.assertNotEqual((), self.time_table.get_departure_times())
-    #
-    # def test_time_should_be_in_correct_format(self):
-    #     time_index = randint(0, len(self.time_table.get_departure_times()) - 1)
-    #     seconds_in_day = 24 * 60 * 60
-    #     result_time = self.time_table.get_departure_times()[time_index]
-    #
-    #     self.assertTrue(
-    #         type(result_time) is int,
-    #         msg='Should be from Clock'
-    #     )
-    #     self.assertGreaterEqual(self.time_table.get_departure_times()[time_index], 0)
-    #     self.assertLess(self.time_table.get_departure_times()[time_index], seconds_in_day)
+    def setUp(self) -> None:
+        self.time_table = TimeTable((Hour('11:15'), Hour('17:23'), Hour('23:10'))
+        )
+
+    def tearDown(self) -> None:
+        del self.time_table
+
+    def test_should_return_departure_times(self):
+        self.assertNotEqual((), self.time_table.get_departure_times())
+
+    def test_time_should_be_in_correct_format(self):
+        time_index = randint(0, len(self.time_table.get_departure_times()) - 1)
+        seconds_in_day = 24 * 60 * 60
+        result_time = self.time_table.get_departure_times()[time_index]
+
+        self.assertTrue(
+            type(result_time) is int,
+            msg='Should be from Clock'
+        )
+        self.assertGreaterEqual(self.time_table.get_departure_times()[time_index], 0)
+        self.assertLess(self.time_table.get_departure_times()[time_index], seconds_in_day)
 
     @mock.patch('hour.hour.Hour')
     def test_should_raise_error_for_too_big_values(self, hour_mock):
@@ -36,7 +38,7 @@ class TimeTableTest(TestCase):
             TimeTable((hour_mock,))
 
     def test_should_raise_error_for_too_big_values_no_mock(self):
-        hour = HourStub('11:15')
+        hour = HourStub('11:20')
 
         with self.assertRaises(ValueError):
             TimeTable((hour,))
